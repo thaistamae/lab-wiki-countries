0396ee37e5a2451ac402d9db4ddba397d0d50bd2
Thais Tamae <thais.tamae@gmail.com>
2022-02-06 11:21:34 -0300

    V2

--- src/components/CountryDetails/index.js
+++ src/components/CountryDetails/index.js
@@ -4,22 +4,47 @@ import countriesData from '../../countries.json';
 export function CountryDetails() {
   const {countryAbrev} = useParams();
 
-    const newArray = countriesData.filter((country) => {
-      return ((country.alpha3Code) === `${countryAbrev}`)
-    })
+  const newArray = countriesData.filter((country) => {
+    return ((country.alpha3Code) === `${countryAbrev}`)
+  })
   
-  console.log(newArray)
+  const borders = newArray[0].borders  
+  console.log(borders)
 
+  function exportBorders(){
+    const bordersList = [];
+    for (let i = 0; i < borders.length; i++){
+      bordersList.push(<p key={borders[i]}><a href="/#">{borders[i]}</a></p>)
+    }
+    return bordersList
+    
+  }
+
+  console.log(exportBorders())
+  
   return (
-    <div style={{height: 700, marginTop: 10, marginBotton: 10, marginLeft: 250, width: 500, display: "flex", flexDirection: "column", alignItems: "flex-start",}}>
+    <div style={{height: 700, width: 1000, marginTop: 10, marginBotton: 10, marginLeft: 20, display: "flex", flexDirection: "column",}}>
         
         {newArray.map((country) => {
           return (
-          <section key={country.alpha3Code}>
-          <img src={`https://flagpedia.net/data/flags/icon/72x54/${country.alpha2Code.toLowerCase()}.png`} alt={`${country.name.common}`}/>
+          <section key={country.alpha3Code} style={{height: 700, width: 800}}>
+            <img src={`https://flagpedia.net/data/flags/icon/72x54/${country.alpha2Code.toLowerCase()}.png`} alt={`${country.name.common}`}/>
             <h1>{country.name.common}</h1>
-            <p>Capital: {country.capital}</p>
-            <p>Area: {country.area} km²</p>
+            <hr></hr>
+            <article style={{display: "flex", justifyContent: "space-around", }}>
+              <p>Capital: </p>
+              <p>{country.capital}</p>
+            </article>
+            <hr></hr>
+            <article style={{display: "flex", justifyContent: "space-around", }}>
+              <p>Area: </p>
+              <p>{country.area} km²</p>
+            </article>
+            <hr></hr>
+            <article style={{display: "flex", justifyContent: "space-around", }}>
+              <p>Borders: </p>
+              <ul>{exportBorders()}</ul>
+            </article>
           </section>
       )})}
     
